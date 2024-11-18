72fdb1a7148807df07c16b4e46a0b8b18d41c61d
shimon <shimiya18@gmail.com>
2024-11-18 22:28:07 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SimpleGame.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SimpleGame.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main() {
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Your rendering code here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
