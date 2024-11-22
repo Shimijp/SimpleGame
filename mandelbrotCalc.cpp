3dfa24a035da3effb03e1aec331d4c53a04e7b11
shimon <shimiya18@gmail.com>
2024-11-22 23:18:36 +0200

    fixed .frag .vert locations

---
+++ mandelbrotCalc.cpp
@@ -0,0 +1,3 @@
+//
+// Created by shimo on 21/11/2024.
+//
---
+++ mandelbrotCalc.h
@@ -0,0 +1,8 @@
+//
+// Created by shimo on 21/11/2024.
+//
+
+#ifndef MANDELBROTCALC_H
+#define MANDELBROTCALC_H
+
+#endif //MANDELBROTCALC_H
